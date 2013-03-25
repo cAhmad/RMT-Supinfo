25300105ff939fe7e940edf303d4b8cab176791c
cAhmad <Ahmad7.974@gmail.com>
2013-03-25 23:30:27 +0100

    add manager property

--- src/main/java/com/supinfo/rmt/entity/Employee.java
+++ src/main/java/com/supinfo/rmt/entity/Employee.java
@@ -2,7 +2,9 @@ package com.supinfo.rmt.entity;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author Ahmad
@@ -12,4 +14,22 @@ import java.io.Serializable;
 @DiscriminatorValue(value= "employee")
 public class Employee extends User implements Serializable {
 
+    @ManyToOne
+    private Manager manager;
+
+    public Employee() {
+    }
+
+    public Employee(String username, String password, String firstName, String lastName, String email, Date dateOfBirth, Manager manager) {
+        super(username, password, firstName, lastName, email, dateOfBirth);
+        this.manager = manager;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
 }
