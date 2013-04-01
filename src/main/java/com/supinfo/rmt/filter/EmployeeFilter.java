f9324aba69fda5a9278156985e99532d01229d12
cAhmad <Ahmad7.974@gmail.com>
2013-04-01 17:35:01 +0200

    Add 2 filters to secure path

---
+++ src/main/java/com/supinfo/rmt/filter/EmployeeFilter.java
@@ -0,0 +1,43 @@
+package com.supinfo.rmt.filter;
+
+import com.supinfo.rmt.entity.Employee;
+import com.supinfo.rmt.entity.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Ahmad
+ * @version $Id$
+ */
+@WebFilter(urlPatterns = "/faces/employee/*")
+public class EmployeeFilter implements Filter {
+
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        User user = (User) httpRequest.getSession().getAttribute("user");
+        if (user == null || !(user instanceof Employee)) {
+            httpResponse.sendRedirect(httpRequest.getServletContext().getContextPath() + "/faces/login.jsf");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
